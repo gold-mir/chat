75960c2d974f62abc18a231c1037ab532f9e5731
gold-mir <miranda.keith.42@gmail.com>
2019-07-12 15:08:59 -0700

    change dummy files slightly

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,8 @@
+import { thing } from './things/thing'
+
 function sayHi(name: String){
   return `Hi ${name}`
 }
 
 console.log(sayHi('Miranda'))
+console.log(thing())
---
+++ src/server/things/thing.ts
@@ -0,0 +1,3 @@
+export function thing():String{
+  return "I'm a girl"
+}
